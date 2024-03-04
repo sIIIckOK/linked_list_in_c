315b3c711f4bd6f9b2bb476e0fed5e7aa76b2b36
sIIIckOK <siddharthtdd123@gmail.com>
2024-03-04 21:22:48 +0545

    Fix git fr this time

--- example.c
+++ example.c
@@ -18,19 +18,3 @@ int main(){
     return 0;
 }
 
-int main(){
-    //LinkedList: type is the type of a linked list
-    LinkedList* l = CreateLinkedList(20);
-    //AppendLinkedList: adds element to the end of linked list
-    AppendLinkedList(l, 120);
-    //AppfrontLinkedList: adds element to the front of linked list
-    AppfrontLinkedList(l, 300);
-                               
-    //length: lists have length to easy loops
-    for (int i = 0; i < l->length; i++){
-        //Node: type is elements within linked lists
-        Node* e = GetElement(l, i);//GetElement: gets element from a linked list
-        printf("index: %d, value: %d\n", i, e->value);
-    }
-    return 0;
-}
--- linkedList.h
+++ linkedList.h
@@ -25,29 +25,3 @@ LinkedList* CreateLinkedList(int v);
 void AppendLinkedList(LinkedList* l, int v);
 
 void AppfrontLinkedList(LinkedList* l, int v);
-
-typedef struct Node{
-    int value;
-    struct Node* prev;
-    struct Node* next;
-}Node;
-
-typedef struct{
-    Node* head;
-    Node* tail;
-    int length;
-}LinkedList;
-
-Node* CreateNode(int v);
-
-Node* AppendNodeToNode(Node* prevNode, int v);
-
-Node* GetElementFromNode(Node* h, int i);
-
-Node* GetElement(LinkedList* l, int i);
-
-LinkedList* CreateLinkedList(int v);
-
-void AppendLinkedList(LinkedList* l, int v);
-
-void AppfrontLinkedList(LinkedList* l, int v);
